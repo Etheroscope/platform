7bd29a5796780e53f7324c34761278288f899416
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-10-16 16:41:48 +0100

    Create elixir-setup.sh

---
+++ elixir-setup.sh
@@ -0,0 +1,8 @@
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update
+yes | sudo apt-get install esl-erlang
+yes | sudo apt-get install elixir
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
+yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
+yes | sudo apt-get install nodejs-legacy
+yes | mix local.hex
